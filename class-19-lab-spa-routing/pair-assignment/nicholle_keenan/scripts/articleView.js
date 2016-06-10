d0f2e3d877a2e89af5d4a17eb18251667224a35d
Keenan <Keenan@Keenans-MBP.lan>
2016-06-10 09:23:49 -0700

    completed stretch goals

--- class-19-lab-spa-routing/pair-assignment/nicholle_keenan/scripts/articleView.js
+++ class-19-lab-spa-routing/pair-assignment/nicholle_keenan/scripts/articleView.js
@@ -116,16 +116,7 @@
     articleView.setTeasers();
   };
 
-  articleView.initAdminPage = function() {
-    var template = Handlebars.compile($('#author-template').text());
-
-    Article.numWordsByAuthor().forEach(function(stat) {
-      $('.author-stats').append(template(stat));
-    });
-
-    $('#blog-stats .articles').text(Article.all.length);
-    $('#blog-stats .words').text(Article.numWordsAll());
-  };
+  
 
   module.articleView = articleView;
 })(window);
